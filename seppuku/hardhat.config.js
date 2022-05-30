41af955217437b3f75c7b6bfccf5a7255b8c5e59
Umut yeşildal <umutyesildal@Umut-MacBook-Air.local>
2022-05-30 23:47:07 +0300

    Hardhat implementation has been added, contract has been deployed and verified

---
+++ seppuku/hardhat.config.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+hardhat: {},
+rinkeby: {
+  url: process.env.API_URL,
+  accounts: [`0x${process.env.PRIVATE_KEY}`],
+}
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+///0x4B5DC975ec2B702131963aD868Ecfc0C0B92448E
